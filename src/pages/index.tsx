071eebeadbe73f2531dda8e7432239324da0bf71
Andre Luiz <adventureandre@hotmail.com>
2023-12-14 20:06:23 -0300

    Inicializando com o Stripe

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,24 +4,50 @@ import Image from "next/image"
 import camiseta1 from '@/assets/camisetas/1.png'
 import camiseta2 from '@/assets/camisetas/2.png'
 import camiseta3 from '@/assets/camisetas/3.png'
+import {useKeenSlider} from "keen-slider/react";
+
+import 'keen-slider/keen-slider.css';
 
 export default function Home() {
+
+  const [sliderRef] = useKeenSlider({
+    slides:{
+      perView:3,
+      spacing:48
+
+    }
+  })
+
   return (
- <HomeConteiner>
-  <Product>
+ <HomeConteiner ref={sliderRef} className="keen-slider">
+  <Product className="keen-slider__slide">
     <Image src={camiseta1} width={520} height={480} alt=""/>
     <footer>
       <strong>Camiseta X</strong>
       <span>R$ 79,90</span>
     </footer>
   </Product>
-  <Product>
+  <Product className="keen-slider__slide">
     <Image src={camiseta1} width={520} height={480} alt=""/>
     <footer>
       <strong>Camiseta X</strong>
       <span>R$ 79,90</span>
     </footer>
   </Product>
+   <Product className="keen-slider__slide">
+     <Image src={camiseta1} width={520} height={480} alt=""/>
+     <footer>
+       <strong>Camiseta X</strong>
+       <span>R$ 79,90</span>
+     </footer>
+   </Product>
+   <Product className="keen-slider__slide">
+     <Image src={camiseta1} width={520} height={480} alt=""/>
+     <footer>
+       <strong>Camiseta X</strong>
+       <span>R$ 79,90</span>
+     </footer>
+   </Product>
  </HomeConteiner>
   )
 }
