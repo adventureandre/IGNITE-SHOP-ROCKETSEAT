6835e3697c10460fbaf4530d51faefa524060f51
Andre Luiz <adventureandre@hotmail.com>
2023-12-18 01:40:02 -0300

    Produto & Checkout - Estrutura de sucesso

--- src/pages/sucess.tsx
+++ src/pages/sucess.tsx
@@ -1,3 +1,20 @@
+import Link from "next/link";
+import { ImageConteiner } from "@/styles/pages/sucess";
+import { SucessConteiner } from "@/styles/pages/sucess";
+
 export default function Sucess() {
-    return <div>sucesss</div>
+    return (
+        <SucessConteiner>
+            <h1>Compra efetuada!</h1>
+            <ImageConteiner>
+
+            </ImageConteiner>
+
+            <p>
+                Uhuul <strong>Andre Luiz</strong>, sua  <strong>Camiseta</strong> já está a caminho da sua casa.
+            </p>
+            <Link href='' >Voltar ao catálago </Link>
+        </SucessConteiner>
+    )
+
 }
