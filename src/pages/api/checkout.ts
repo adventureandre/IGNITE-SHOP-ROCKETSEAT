e4125596672fe18b274ccff563e0cc3fe07054b1
Andre Luiz <adventureandre@hotmail.com>
2023-12-18 22:12:01 -0300

    test

--- src/pages/api/checkout.ts
+++ src/pages/api/checkout.ts
@@ -8,6 +8,8 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
 
   if(req.method !== 'POST'){
     return res.status(405).json({error: 'Method not Allowed.',url:process.env.NEXT_URL});
+
+    
   }
 
   if(!priceId){
